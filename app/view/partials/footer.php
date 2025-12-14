e52ae66b70bfbc1bd8d995f1729c7e1b37beed4f
Pol Moreno Queraltó <polmorenofp23@ibf.cat>
2025-12-14 21:24:18 +0100

    Footer completament implementat per a totes les pagines

--- app/view/partials/footer.php
+++ app/view/partials/footer.php
@@ -1,5 +1,72 @@
 <footer>
-    <div class="main-footer">
+    <div class="main-footer d-flex flex-column justify-content-center align-items-center">
+        <div class="quality-logos red-bg d-flex flex-column justify-content-center align-items-center ">
+            <div class="row mb-3">
+                <h3 class="text-uppercase">OFFICIAL QUALITY STAMPTS</h3>
+            </div>
+            <div class="row justify-content-between align-items-center my-5">
+                <div class="col-6 col-sm-4 col-md-2 d-flex justify-content-center">
+                    <img src="/assets/img/logo-meat_quality.webp" alt="Quality logo 1">
+                </div>
+                <div class="col-6 col-sm-4 col-md-2 d-flex justify-content-center">
+                    <img src="/assets/img/logo.png" alt="Quality logo 2">
+                </div>
+                <div class="col-6 col-sm-4 col-md-2 d-flex justify-content-center">
+                    <img src="/assets/img/logo-real-food-1.jpg" alt="Quality logo 3">
+                </div>
+            </div>
+            <div class="row col-10 my-4">
+                <hr class="border border-white border-1 opacity-50">
+            </div>
+        </div>
+        <div class="footer-info-box text-dark mb-5">
+            <div class="container p-5">
+                <div class="row">
+                    <div class="col-12 col-md-6 mb-4">
+                        <img src="/assets/img/logo.png" alt="Bees Cavern Logo" class="foot-logo">
+                        <h5 class="text-tertiary-red mt-3">BEES CAVERN RESTAURANT</h5>
+                        <address class="d-block my-4">
+                            Vantage London<br>
+                            Great West Road<br>
+                            Brentford, London<br>
+                            TW8 9AG
+                        </address>
+                        <a href="#" class="d-block text-align-center text-decoration-none my-5">Get Directions<i data-lucide="chevron-right" class=" icon-red ms-3"></i></a>
+                        <div class="d-flex gap-3 align-items-center">
+                            <a href="#" class="text-primary-red" aria-label="Facebook"><i data-lucide="facebook" class="icon-red"></i></a>
+                            <a href="#" class="text-primary-red" aria-label="Twitter"><i data-lucide="twitter" class="icon-red"></i></a>
+                            <a href="#" class="text-primary-red" aria-label="YouTube"><i data-lucide="youtube" class="icon-red"></i></a>
+                            <a href="#" class="text-primary-red" aria-label="Instagram"><i data-lucide="instagram" class="icon-red"></i></a>
+                            <a href="#" class="text-primary-red" aria-label="LinkedIn"><i data-lucide="linkedin" class="icon-red"></i></a>
+                        </div>
+                    </div>
 
+                    <div class="col-6 col-md-3">
+                        <h6 class="mb-4">Restaurant</h6>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="d-block py-2 text-decoration-none">Our Values</a></li>
+                            <li><a href="#" class="d-block py-2 text-decoration-none">Be Membership</a></li>
+                            <li><a href="#" class="d-block py-2 text-decoration-none">History</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-6 col-md-3">
+                        <h6 class="mb-4">Community</h6>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="d-block py-2 text-decoration-none">Brentford FC Vinculation</a></li>
+                            <li><a href="#" class="d-block py-2 text-decoration-none">Events</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row bg-color-white p-5 m-1">
+                    <div class="col-12">
+                        <div class="d-flex flex-column flex-md-row justify-content-between align-items-center small ">
+                            <div class="mb-2 mb-md-0"><span>Privacy Policy &nbsp;|&nbsp; Terms And Conditions &nbsp;|&nbsp; FAQs &nbsp;|&nbsp; Policies And Regulations</span></div>
+                            <div class="small">&copy; 2026 Bees Cavern. All rights reserved.</div>
+                        </div>
+                    </div>
+                </div>
+        </div>
     </div>
 </footer>
