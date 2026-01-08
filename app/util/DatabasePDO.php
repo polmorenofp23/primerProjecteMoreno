81753b63f0975b202ff057a200e319929bf1dde3
Pol Moreno Queraltó <polmorenofp23@ibf.cat>
2026-01-08 11:53:14 +0100

    Adaptacio del DatabasePDO i creacio dels dockerfiles i docker-compose per aixecar els contenidors necessaris per hostejar la Web i la BD

--- app/util/DatabasePDO.php
+++ app/util/DatabasePDO.php
@@ -2,16 +2,22 @@
 
 // Class of the database PHP Data Object, that allow us to connetct and close connection with DB that will use the DAO models to obtain the data.
 class DatabasePDO{
-    // Variables that contain the connection data
-    private $host = 'localhost';
-    private $dbname = 'bees_cavern_db';
-    private $user = 'root';
-    private $password = 'Asdqwe!23';
+    // Variables that contain the connection data 
+    private $host;                  // 'localhost'
+    private $dbname;                // 'bees_cavern_db'
+    private $user;                  // 'polmq'
+    private $password;              // 'Asdqwe!23'
 
     private $conn = null;
 
     public function __construct()
     {
+        // IMPORTANT: same variables values that contains the docker-compose.yml
+        $this->host     = getenv('DB_HOST')     ?: 'db';              // service name in docker-compose
+        $this->dbname   = getenv('DB_NAME')     ?: 'bees_cavern_db';
+        $this->user     = getenv('DB_USER')     ?: 'polmq';
+        $this->password = getenv('DB_PASSWORD') ?: 'Asdqwe!23';
+
         $this->initializeConnection();
     }
 
@@ -44,7 +50,7 @@ class DatabasePDO{
      * realise the connection to the database
      */
     public function connect() : PDO {
-        if (!($this->conn instanceof PDO)) {                                                        // Lazy (re)connect: if previously disconnected, recreate the PDO instance.
+        if (!($this->conn instanceof PDO)) {    // Lazy (re)connect: if previously disconnected, recreate the PDO instance
             $this->initializeConnection();
         }
         return $this->conn;
